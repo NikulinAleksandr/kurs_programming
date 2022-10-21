06e0e5a2bd0f5eda8c459d086e521cf74fdf153a
Aleksandr <nikulinaleksandr@mail.ru>
2022-10-21 10:14:12 +0500

    решили первую задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+    /*
+        int num1, num2;    
+        Console.WriteLine("Введите два числа: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        num2 = Convert.ToInt32(Console.ReadLine());
+        if (num1 > num2)
+        {
+            Console.WriteLine("{0} наибольшее число", num1);
+            Console.WriteLine("{0} наименьшее число", num2);
+        }
+        else
+        {
+            Console.WriteLine("{0} наибольшее число", num2);
+            Console.WriteLine("{0} наименьшее число", num1);
+        }
+        Console.ReadLine();
+*/
+      
