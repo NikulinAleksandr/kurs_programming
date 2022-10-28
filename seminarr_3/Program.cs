a1fae0f03cbdaa61072a2a8a8794c261b4199515
Aleksandr <nikulinaleksandr@mail.ru>
2022-10-28 12:47:46 +0500

    Решили первую задачу

--- seminarr_3/Program.cs
+++ seminarr_3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
+Console.Write("Введите число: ");
+string text = Console.ReadLine();
+int a = text.Length;
+bool x = true;
+for (int i = 0; i < a/2; i++)
+  {
+    if (text [i] != text [a - (i + 1)])
+    {
+        x = false;
+        break;
+    }
+  }
+    if (x)
+    {
+        Console.WriteLine("{0} это палиндромом", text);
+    }
+    else
+    {
+        Console.WriteLine("{0} это не палиндромом", text);
+    }
+  */
